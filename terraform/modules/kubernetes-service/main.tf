95744bb5148d0f165ecd976761f1c89202b0f5a8
chengqi.li <chengqi.li@cacloudstudio.com>
2025-08-05 16:32:18 -0700

    Add Azure Container Registry provisioning tied to Kubernetes Service

--- terraform/modules/kubernetes-service/main.tf
+++ terraform/modules/kubernetes-service/main.tf
@@ -4,17 +4,21 @@ resource "azurerm_resource_group" "resource_group" {
   location = each.value.location
 }
 
-resource "tls_private_key" "ssh" {
-  for_each  = var.azure_kubernetes_service
-  algorithm = "RSA"
-  rsa_bits  = 4096
+resource "azurerm_container_registry" "acr" {
+  for_each            = var.azure_kubernetes_service
+  name                = "${each.key}acr1000"
+  resource_group_name = lookup(azurerm_resource_group.resource_group, each.key).name
+  location            = each.value.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
 
-resource "azurerm_key_vault_secret" "aks_ssh_private_key" {
-  for_each     = var.azure_kubernetes_service
-  name         = "${each.key}-ssh-private-key"
-  value        = tls_private_key.ssh[each.key].private_key_pem
-  key_vault_id = var.key_vault[each.key].id
+resource "azurerm_role_assignment" "acr_pull" {
+  depends_on           = [azurerm_kubernetes_cluster.kubernetes_service]
+  for_each             = var.azure_kubernetes_service
+  principal_id         = azurerm_kubernetes_cluster.kubernetes_service[each.key].kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr[each.key].id
 }
 
 resource "azurerm_kubernetes_cluster" "kubernetes_service" {
@@ -34,13 +38,13 @@ resource "azurerm_kubernetes_cluster" "kubernetes_service" {
     min_count                   = each.value.default_node_pool.min_count
   }
 
-  linux_profile {
-    admin_username = each.value.linux_profile.admin_username
+  # linux_profile {
+  #   admin_username = each.value.linux_profile.admin_username
 
-    ssh_key {
-      key_data = tls_private_key.ssh[each.key].public_key_openssh
-    }
-  }
+  #   ssh_key {
+  #     key_data = tls_private_key.ssh[each.key].public_key_openssh
+  #   }
+  # }
 
   identity {
     type = "SystemAssigned"
@@ -50,3 +54,16 @@ resource "azurerm_kubernetes_cluster" "kubernetes_service" {
     network_plugin = "azure"
   }
 }
+
+# resource "tls_private_key" "ssh" {
+#   for_each  = var.azure_kubernetes_service
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
+
+# resource "azurerm_key_vault_secret" "aks_ssh_private_key" {
+#   for_each     = var.azure_kubernetes_service
+#   name         = "${each.key}-ssh-private-key"
+#   value        = tls_private_key.ssh[each.key].private_key_pem
+#   key_vault_id = var.key_vault[each.key].id
+# }
